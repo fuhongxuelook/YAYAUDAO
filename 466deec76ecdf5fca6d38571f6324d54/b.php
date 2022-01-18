fc798dc591e21a792ecdfbf154b17cf85ebd9be9
mengmengf <mengmengf@opera.com>
2022-01-18 19:10:58 +0800

    midofy

---
+++ 466deec76ecdf5fca6d38571f6324d54/b.php
@@ -0,0 +1,34 @@
+<?php
+
+
+function makeJsonDoc($num) {
+  $newnum=str_pad($num,4,"0",STR_PAD_LEFT);  
+
+  // $doc = '{"attributes": [],"name": "YAYAU DAO#' . $newnum . '","description": "YAYAUDAO is a communication project that aims to revolutionize the way how fans and popular stars interact.\tYAYAUDAO establishes communication for all collaborators.We started from a sticker pack, but more excitement will happen. \tOur core values are caring, expression and infinity.","image": "https://cloudflare-ipfs.com/ipfs/bafybeicjctq6eyfxkuud72z5m2lbon7mbkknkytq24rqddyqbe6bsw2tvq/metadata/'. $num.'.json"}';
+
+    // direct to cover
+  $doc = '{"attributes": [],"name": "YAYAU DAO#' . $newnum . '","description": "YAYAUDAO is a communication project that aims to revolutionize the way how fans and popular stars interact.\tYAYAUDAO establishes communication for all collaborators.We started from a sticker pack, but more excitement will happen. \tOur core values are caring, expression and infinity.","image": "https://yayaudao.com/ff432c6e08f2a9ce6a5584db579cae99/' . $newnum . '.jpg"}';
+
+  var_dump($doc);
+  $myfile = fopen($num . ".json", "w") or die("Unable to open file!");
+  fwrite($myfile, $doc);
+  fclose($myfile);
+
+}
+
+
+function make() {
+  $i = 1;
+  while($i < 3) {
+    makeJsonDoc($i);
+    $i++;
+  }
+}
+make() ;
+
+
+
+?>
+
+//https://arweave.net/8HgUNGhUGy5CnT8gAQgr1nlKfjmNRY9ljCKw6Veom-0
+// 30000000000000000
